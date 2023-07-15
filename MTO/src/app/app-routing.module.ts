322b2a836c47f83ea100d5ebbe08ba193fc67f78
Dmitry Ivanov <73381002+daug32@users.noreply.github.com>
2023-07-15 22:16:59 +0300

    Маршрутизация
    * Добавлена пустая страница os_usage для показа примера того, как происходит маршрутизация (#1)
    * Добавлена сонова для маршрутизации

--- MTO/src/app/app-routing.module.ts
+++ MTO/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CategoriesComponent } from "./handbook_pages/categories/categories.component";
+import { OsUsageComponent } from "./handbook_pages/os-usage/os-usage.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  // Handbook
+  { path: 'OsCategories', component: CategoriesComponent },
+  { path: 'OsUsage', component: OsUsageComponent },
+
+  // Default route
+  { path: '', component: CategoriesComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
